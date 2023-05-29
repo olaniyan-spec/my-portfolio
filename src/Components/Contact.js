f29942267c8bebc8d9c5c658409de6c2a1d56ced
Tofunmi <olaniyanolaoluwa3@gmail.com>
2023-05-29 21:23:24 +0100

    new commit

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,29 +1,26 @@
 import React from 'react'
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button'
 import {FaLinkedin} from 'react-icons/fa'
 import {AiOutlineTwitter} from 'react-icons/ai'
 import {AiFillInstagram} from 'react-icons/ai'
+import { MdEmail } from 'react-icons/md'
+import { IoLogoWhatsapp } from 'react-icons/io'
+
 
 const Contact = () => {
+  
   return (
     <section className='contact' id="Contact">
         <h1>Contact Me</h1>
-        <div className='contact-wrapper'>
-            <form >
-        <TextField sx={{ background:'transparent' ,width: 350}} margin="normal" id="standard-basic" label="First Name" variant="standard" />
-        <TextField sx={{ background:'transparent' ,width: 350}} margin="normal" id="standard-basic" label=" Last Name" variant="standard" />
-        <TextField sx={{ backgroundColor:'', width: 350}} margin="normal" id="standard-basic" label="Phone Number" variant="standard" />
-        <TextField sx={{ backgroundColor:'', width: 350}} margin="normal" id="standard-basic" label="Email Address" variant="standard" />
-            </form>
-        </div>
-        <Button  type="submit" variant="contained">Submit</Button>
+        <div className='contact-wrapper'> 
         <div className='socials'>
-        <a href='https://linkedin.com/' target='_blank' rel='noreferrer noopener'><FaLinkedin/></a>
+                <a href='https://linkedin.com/' target='_blank' rel='noreferrer noopener'><FaLinkedin/></a>
                 <a href='https://twitter.com/@TofunmiOlaniyan' target='_blank' rel='noreferrer noopener'><AiOutlineTwitter/></a>
                 <a href='https://instagram.com/tofunmi_olaniyan' target='_blank' rel='noreferrer noopener'><AiFillInstagram/></a>
+                <a href='mailto:olaniyanolaoluwa3@gmail.com' target='blank' rel='noreferrer noopener'><MdEmail/></a>
+                <a href='https://wa.me/+2348109799958' target='blank' rel='noreferrer noopener'><IoLogoWhatsapp/></a>
+                
         </div>
-               
+              </div> 
     </section>
   )
 }
--- src/data.js
+++ src/data.js
@@ -6,15 +6,21 @@ export const project = [
         link: 'https://healthy-workout.netlify.app/'
     },
     {
-        id: 1,
+        id: 2,
         image: require('./images/fan.png'),
         title: 'E-commerce',
         link: 'https://tofunmi-fan-app.netlify.app/'
     },
     {
-        id: 1,
+        id: 3,
         image: require('./images/nftimage.png'),
         title: 'Simple NFt',
         link: 'https://nft-tofunmi.netlify.app/'
+    },
+    {
+        id: 4,
+        image: require('./images/construction.jpg'),
+        title: 'Construction Company Landing Page',
+        link: 'https://construction-tf.vercel.app/'
     },
 ]
